7ec79a91ae4c231c4fd637c6a2b5fc1af0215da2
Sorawa <thepowerover@163.com>
2017-09-18 10:32:13 +0800

    add http api to add token

--- utils/bucket/bucket.go
+++ utils/bucket/bucket.go
@@ -1,8 +1,11 @@
 package bucket
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -27,6 +30,29 @@ func ListAuth() {
 	for k, v := range _authInfo {
 		fmt.Printf("Key : %s Tokey %s \n", k, v)
 	}
+}
+
+func HttpServer(addr string) {
+	http.HandleFunc("/add", func(w http.ResponseWriter, r *http.Request) {
+		key := r.FormValue("key")
+		token := r.FormValue("token")
+		fmt.Printf("add key %v  and token %v \n", key, token)
+		SetAuth(key, token)
+
+		_, err := os.Stat("./auth/" + key)
+		if err != nil {
+			ioutil.WriteFile("./auth/"+key, []byte(token), os.ModeAppend)
+		}
+
+		retObj := map[string]interface{}{
+			"ret_code": 0,
+			"message":  "success",
+		}
+		retByt, _ := json.Marshal(retObj)
+		w.Write(retByt)
+	})
+
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func init() {
